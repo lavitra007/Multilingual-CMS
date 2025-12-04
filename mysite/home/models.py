4f6b0c39804202569a990f0f84c2d04a8ebff59d
AdityaSharma-Git3207 <aadee.bharat@gmail.com>
2025-12-05 01:35:56 +0530

    initial code commit

---
+++ mysite/home/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class HomePage(Page):
+    subtitle = models.CharField(max_length=255, blank=True)
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('banner_image'),
+        FieldPanel('body'),
+    ]
